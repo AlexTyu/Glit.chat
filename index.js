e4d6721a448a90e2dfe62eb1ff51d978c0b6486a
Sergey Samusenko <sergey.samusenko@inpwrd.com>
2017-02-23 14:17:06 -0800

    refactor

--- index.js
+++ index.js
@@ -12,266 +12,76 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-var messages = [
+var MESSAGES = [
     {
         user: "1",
         text: "[103.231.481.213]. Ping 424ms",
-        color: "gray",
+        textColor: "gray",
     },
     {
         user: "1",
         text: "Connection established",
-        color: "gray",
+        textColor: "gray",
     },
     {
         user: "0",
         text: "ОLEГ",
-        color: "red",
-        size: "40"
+        textColor: "red",
+        textSize: "40"
     },
     {
         user: "0",
         text: "Are you there?",
-        color: "red"
+        textColor: "red"
     },
     {
         user: "0",
         text: "Why do you think you can trust them?",
-        color: "red"
+        textColor: "red"
     }
 ];
-var popup = false;
+
+var OPTIONS = {
+    textColor: "#000000",
+    textSize: 16,
+    bgColor: "#000000",
+    bgImage: "none"
+};
 
 var TEXT_COLOR = "#f00";
-var SIZE = 16;
-var BGIMAGE = "";
-var BGCOLOR = "";
-var POPUP = false;
-var POPUP_TEXT = "Pizdec tebe, OLEfГ"
-
-var WRANIM = "";
-var LIVECSS = "";
-
-var messageText = ""
 
 io.on('connection', function (socket) {
 
     function emitMessages() {
         socket.emit('messages', {
-            messages: messages
+            messages: MESSAGES
         });
         socket.broadcast.emit('messages', {
-            messages: messages
+            messages: MESSAGES
         });
     }
-
 
     function emitOptions() {
-
-        socket.emit('size', {
-            size: SIZE
+        socket.emit('options', {
+            options: OPTIONS
         });
-        socket.broadcast.emit('size', {
-            size: SIZE
+        socket.broadcast.emit('options', {
+            options: OPTIONS
         });
-        socket.emit('bgimage', {
-            bgimage: BGIMAGE
-        });
-        socket.broadcast.emit('bgimage', {
-            bgimage: BGIMAGE
-        });
-
-        socket.emit('adminTextColor', {
-            adminTextColor: TEXT_COLOR
-        });
-
-        socket.broadcast.emit('textcolor', {
-            textcolor: TEXT_COLOR
-        });
-
-        socket.emit('bgcolor', {
-            bgcolor: BGCOLOR
-        });
-        socket.broadcast.emit('bgcolor', {
-            bgcolor: BGCOLOR
-        });
-
-        socket.emit('popup', {
-            popup: POPUP
-        });
-        socket.broadcast.emit('popup', {
-            popup: POPUP
-        });
-
-        socket.emit('popupText', {
-            popupText: POPUP_TEXT
-        });
-        socket.broadcast.emit('popupText', {
-            popupText: POPUP_TEXT
-        });
-
-        socket.emit('wrapperAnimation', {
-            wrapperAnimation: WRANIM
-        });
-        socket.broadcast.emit('wrapperAnimation', {
-            wrapperAnimation: WRANIM
-        });
-
-        socket.emit('LiveCss', {
-            LiveCss: LIVECSS
-        });
-        socket.broadcast.emit('LiveCss', {
-            LiveCss: LIVECSS
-        });
-
-        socket.emit('messageText', {
-            messageText: messageText
-        });
-        socket.broadcast.emit('messageText', {
-            messageText: messageText
-        });
-
-
     }
 
+    socket.on('options', function(data) {
+        OPTIONS = data;
+        emitOptions();
+    });
+
     socket.on('new message', function(message) {
-        messages.push(message);
+        MESSAGES.push(message);
         emitMessages();
     });
-
-
-    socket.on('adminTextColor', function(data) {
-        TEXT_COLOR = data.adminTextColor;
-        emitOptions();
-    });
-
-    socket.on('bgcolor', function(data) {
-        BG_COLOR = data.bgcolor;
-        emitOptions();
-    });
-
-    socket.on('size', function(data) {
-        SIZE = data.size;
-        emitOptions();
-    });
-
-    socket.on('bgimage', function(data) {
-        BGIMAGE = data.bgimage;
-        emitOptions();
-    });
-
-    socket.on('popup', function(data) {
-        POPUP = data.popup;
-        emitOptions();
-    });
-
-    socket.on('popupText', function(data) {
-        POPUP_TEXT = data.popupText;
-        emitOptions();
-    });
-
-    socket.on('messageText', function(data) {
-        messageText = data.messageText;
-        emitOptions();
-    });
-
-    socket.on('wrapperAnimation', function(data) {
-        WRANIM = data.wrapperAnimation;
-        emitOptions();
-    });
-
-    socket.on('LiveCss', function(data) {
-        LIVECSS = data.LiveCss;
-        emitOptions();
-    });
-
-
 
 
     emitMessages();
     emitOptions();
 
-
-  //
-  //   socket.emit('avatars', {
-  //       avatars: getAvatars()
-  //   });
-  //
-  //   socket.on('register', function(user) {
-  //       var _user = new User(user);
-  //       _users.push(_user);
-  //       _availableAvatars.splice(_availableAvatars.indexOf(_user.avatar.name), 1);
-  //       if(socket.avatar) {
-  //           _availableAvatars.push(socket.avatar);
-  //       }
-  //       socket.avatar = _user.avatar.name;
-  //       socket.emit('avatars', {
-  //           avatars: getAvatars()
-  //       });
-  //       socket.broadcast.emit('avatars', {
-  //           avatars: getAvatars()
-  //       });
-  //   });
-  //
-  //   socket.on('disconnect', function() {
-  //       _availableAvatars.push(socket.avatar);
-  //       socket.broadcast.emit('avatars', {
-  //           avatars: getAvatars()
-  //       });
-  //   });
-  //
-  // // when the client emits 'new message', this listens and executes
-  // socket.on('new message', function (data) {
-  //   // we tell the client to execute 'new message'
-  //   socket.broadcast.emit('new message', {
-  //       username: socket.username,
-  //       message: data,
-  //       date: new Date()
-  //   });
-  // });
-  //
-  // // when the client emits 'add user', this listens and executes
-  // socket.on('add user', function (username) {
-  //   if (addedUser) return;
-  //
-  //   // we store the username in the socket session for this client
-  //   socket.username = username;
-  //   ++numUsers;
-  //   addedUser = true;
-  //   socket.emit('login', {
-  //     numUsers: numUsers
-  //   });
-  //   // echo globally (all clients) that a person has connected
-  //   socket.broadcast.emit('user joined', {
-  //     username: socket.username,
-  //     numUsers: numUsers
-  //   });
-  // });
-  //
-  // // when the client emits 'typing', we broadcast it to others
-  // socket.on('typing', function () {
-  //   socket.broadcast.emit('typing', {
-  //     username: socket.username
-  //   });
-  // });
-  //
-  // // when the client emits 'stop typing', we broadcast it to others
-  // socket.on('stop typing', function () {
-  //   socket.broadcast.emit('stop typing', {
-  //     username: socket.username
-  //   });
-  // });
-  //
-  // // when the user disconnects.. perform this
-  // socket.on('disconnect', function () {
-  //   if (addedUser) {
-  //     --numUsers;
-  //
-  //     // echo globally that this client has left
-  //     socket.broadcast.emit('user left', {
-  //       username: socket.username,
-  //       numUsers: numUsers
-  //     });
-  //   }
-  // });
 });
--- public/js/params.js
+++ public/js/params.js
@@ -1,137 +1,40 @@
 var app = angular.module("beetchat", []);
+var Socket = io();
+
 
 app.controller("main", function($scope) {
-    $scope.socket = io();
-    $scope.message = "";
     $scope.user = 0;
-    $scope.bgimage = "none";
-    $scope.bgcolor = "black";
-    $scope.adminTextColor = "red";
-    $scope.clientTextColor = "white";
-
-    $scope.popup = false;
-    $scope.popupText = "PIZDEC TEBE, OLEГ";
 
     var $messages = document.getElementById('messages');
 
-    $scope.socket.on('messages', function(data) {
+    Socket.on('messages', function(data) {
         $scope.messages = data.messages;
         $scope.$apply();
         $messages.scrollTop = $messages.scrollHeight;
     })
 
+    $scope.updateOptions = function() {
+        Socket.emit("options", {
+            textColor: $scope.options.textColor,
+            textSize: $scope.options.textSize,
+            bgColor: $scope.options.bgColor,
+            bgImage: $scope.options.bgImage
+        });
+    }
+
+    Socket.on('options', function(data) {
+        $scope.options = data.options;
+        $scope.$apply();
+    })
+
     $scope.sendMessage = function() {
-        $scope.socket.emit("new message", {
+        Socket.emit("new message", {
             user: $scope.user,
-            text: $scope.messageText,
-            color: $scope.adminTextColor,
-            size: $scope.messageSize
+            text: $scope.message.text,
+            textColor: $scope.message.textColor,
+            textSize: $scope.message.textSize
         });
-        $scope.message = "";
-        $scope.messageText = "";
+        $scope.message.text = "";
     }
-
-    $scope.socket.on('popup', function(data) {
-        $scope.popup = data.popup;
-        $scope.$apply();
-    })
-
-
-
-    $scope.updateSize = function() {
-        $scope.socket.emit("size", {
-            size: $scope.size
-        });
-    }
-
-    $scope.socket.on('size', function(data) {
-        $scope.size = data.size;
-        $scope.$apply();
-    })
-
-    $scope.updateBgImage = function() {
-        $scope.socket.emit("bgimage", {
-            bgimage: $scope.bgimage
-        });
-    }
-
-    $scope.socket.on('bgimage', function(data) {
-        $scope.bgimage = data.bgimage;
-        $scope.$apply();
-    })
-
-    $scope.updateBgColor = function() {
-        $scope.socket.emit("bgcolor", {
-            bgcolor: $scope.bgcolor
-        });
-    }
-
-    $scope.socket.on('bgcolor', function(data) {
-        $scope.bgcolor = data.bgcolor;
-        $scope.$apply();
-    })
-
-    $scope.updateTextColor = function() {
-        $scope.socket.emit("textcolor", {
-            adminTextColor: $scope.adminTextColor
-        });
-    }
-    $scope.socket.on('adminTextColor', function(data) {
-        $scope.adminTextColor = data.adminTextColor;
-        $scope.$apply();
-    })
-
-    $scope.showPopup = function() {
-        $scope.socket.emit("popup", {
-            popup: $scope.popup,
-        });
-    }
-    $scope.socket.on('popup', function(data) {
-        $scope.popup = data.popup;
-        $scope.$apply();
-    })
-
-
-    $scope.updatePopupText = function() {
-        $scope.socket.emit("popupText", {
-            popupText: $scope.popupText,
-        });
-    }
-    $scope.socket.on('popupText', function(data) {
-        $scope.popupText = data.popupText;
-        $scope.$apply();
-    })
-
-
-    $scope.updateMessageText = function() {
-        $scope.socket.emit("messageText", {
-            messageText: $scope.messageText,
-        });
-    }
-    $scope.socket.on('messageText', function(data) {
-        $scope.messageText = data.messageText;
-        $scope.$apply();
-    })
-
-
-    $scope.submitAnimation = function() {
-        $scope.socket.emit("wrapperAnimation", {
-            wrapperAnimation: $scope.wrapperAnimation
-        });
-    }
-    $scope.socket.on('wrapperAnimation', function(data) {
-        $scope.wrapperAnimation = data.wrapperAnimation;
-        $scope.$apply();
-    })
-
-    $scope.submitCss = function() {
-        $scope.socket.emit("LiveCss", {
-            LiveCss: $scope.LiveCss
-        });
-    }
-    $scope.socket.on('LiveCss', function(data) {
-        $scope.LiveCss = data.LiveCss;
-        $scope.$apply();
-    })
 
 });
